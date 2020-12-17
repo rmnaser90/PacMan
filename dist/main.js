3e08d7426691eb4dd19f98673428c5aea73245c7
rmnaser90 <rmnaser90@gmail.com>
2020-12-17 09:28:09 +0200

    all extensions done

--- dist/main.js
+++ dist/main.js
@@ -1,29 +1,30 @@
-const game = new PacMan()
+const game = new PacMan
 const renderer = new Renderer
 
-$("#start").on('click', function() {
-    const rows = $('#rows').val()
+$("#start").on('click', function () {
+    const rows = $('#rows').val() || 18
     $('#rows').val('')
-    const columns = $('#columns').val()
+    const columns = $('#columns').val() || 40
     $('#columns').val('')
-
     game.generateGame(rows, columns)
     renderer.render(game.getBoard())
-    renderer.setBoard(columns)
 })
 
-$('body').on('keypress', function(e) {
+$('body').on('keypress', function (e) {
     if (game.startGame) {
         switch (e.key) {
             case "a":
                 game.moveLeft(game.player1)
                 break;
+
             case 'd':
                 game.moveRight(game.player1)
                 break;
+
             case 'w':
                 game.moveUp(game.player1)
                 break;
+
             case 's':
                 game.moveDown(game.player1)
                 break;
@@ -31,20 +32,24 @@ $('body').on('keypress', function(e) {
             case "j":
                 game.moveLeft(game.player2)
                 break;
+
             case 'l':
                 game.moveRight(game.player2)
                 break;
+
             case 'i':
                 game.moveUp(game.player2)
                 break;
+
             case 'k':
                 game.moveDown(game.player2)
-                break;
-
-            default:
                 break;
         }
         renderer.render(game.getBoard())
         renderer.renderScore(game.getCoins())
+        if (game.checkGameOver()) {
+            renderer.renderGameOver(game.getWinner())
+
+        }
     }
 })
--- dist/renderer.js
+++ dist/renderer.js
@@ -2,28 +2,38 @@ class Renderer {
     constructor() {
         const source = $('#blocks-template').html()
         this.template = Handlebars.compile(source)
-        this.board = $('#board')
-        this.rowsElement = $('')
+        const gameOverSource = $('#gameOver-template').html()
+        this.gameOverTemplate = Handlebars.compile(gameOverSource)
+        this.boardContainer = $('#boardContainer')
         this.columns = 0
+        this.styleSheet = document.styleSheets
     }
 
+    
     setBoard(columns) {
-        this.columns = columns
         const rowsElements = $('.row')
         for (const row of rowsElements) {
             row.style.gridTemplateColumns = `repeat(${columns}, 1fr)`
         }
     }
+
     renderScore(coins) {
         $('#p1-coins').text(`Player 1: ${coins.p1}`)
         $('#p2-coins').text(`Player 2: ${coins.p2}`)
     }
 
     render(board) {
-        this.board.empty()
+        this.columns = board[0].length
+        this.boardContainer.empty()
         const html = this.template({ board })
-        this.board.append(html)
+        this.boardContainer.append(html)
         this.setBoard(this.columns)
-
+        setTimeout(() => {
+            this.styleSheet[0].cssRules[12].style.animationName = 'none'
+        }, 1000)
+    }
+    renderGameOver(player){
+        const html = this.gameOverTemplate(player)
+        this.boardContainer.append(html)
     }
 }
