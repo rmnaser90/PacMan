bc9bc5c76a2cd78a39eca6e581d5f992f1251631
rmnaser90 <rmnaser90@gmail.com>
2020-12-17 13:43:45 +0200

    return to the older version

--- dist/main.js
+++ dist/main.js
@@ -14,73 +14,41 @@ $("#start").on('click', function () {
 
 $('body').on('keypress', function (e) {
     if (game.startGame) {
-        let newGame
         switch (e.key) {
             case "a":
                 game.moveLeft(game.player1)
-                newGame = { ...game }
-                newGame.player2.position = undefined
-                newGame.player2.coins = undefined
-                socket.emit('game', newGame)
-
                 break;
 
             case 'd':
                 game.moveRight(game.player1)
-                newGame = { ...game }
-                newGame.player2.position = undefined
-                newGame.player2.coins = undefined
-                socket.emit('game', newGame)
                 break;
 
             case 'w':
                 game.moveUp(game.player1)
-                newGame = { ...game }
-                newGame.player2.position = undefined
-                newGame.player2.coins = undefined
-                socket.emit('game', newGame)
                 break;
 
             case 's':
                 game.moveDown(game.player1)
-                newGame = { ...game }
-                newGame.player2.position = undefined
-                newGame.player2.coins = undefined
-                socket.emit('game', newGame)
                 break;
 
             case "j":
                 game.moveLeft(game.player2)
-                newGame = { ...game }
-                newGame.player1.position = undefined
-                newGame.player1.coins = undefined
-                socket.emit('game', newGame)
                 break;
 
             case 'l':
                 game.moveRight(game.player2)
-                newGame = { ...game }
-                newGame.player1.position = undefined
-                newGame.player1.coins = undefined
-                socket.emit('game', newGame)
                 break;
 
             case 'i':
                 game.moveUp(game.player2)
-                newGame = { ...game }
-                newGame.player1.position = undefined
-                newGame.player1.coins = undefined
-                socket.emit('game', newGame)
                 break;
 
             case 'k':
                 game.moveDown(game.player2)
-                newGame = { ...game }
-                newGame.player1.position = undefined
-                newGame.player1.coins = undefined
-                socket.emit('game', newGame)
+
                 break;
         }
+        socket.emit('game', game)
 
 
     }
@@ -93,10 +61,10 @@ socket.on('game', function (newGame) {
     game.board.matrix = newGame.board.matrix
     game.columns = newGame.columns
     game.rows = newGame.rows
-    game.player1.coins = newGame.player1.coins || game.player1.coins
-    game.player2.coins = newGame.player2.coins || game.player2.coins
-    game.player1.position = newGame.player1.position || game.player1.position
-    game.player2.position = newGame.player2.position || game.player2.position
+    game.player1.coins = newGame.player1.coins
+    game.player2.coins = newGame.player2.coins
+    game.player1.position = newGame.player1.position
+    game.player2.position = newGame.player2.position
     game.startGame = newGame.startGame
     game.totalCoins = newGame.totalCoins
 
