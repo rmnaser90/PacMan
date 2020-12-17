aafd99718863081634284c68cc31fe84ebc02c78
rmnaser90 <rmnaser90@gmail.com>
2020-12-17 13:29:21 +0200

    bug potentially fixed , first tial on two devices

--- dist/main.js
+++ dist/main.js
@@ -9,46 +9,80 @@ $("#start").on('click', function () {
     $('#columns').val('')
     game.generateGame(rows, columns)
     renderer.render(game.getBoard())
-    socket.emit('game',game)
+    socket.emit('game', game)
 })
 
 $('body').on('keypress', function (e) {
     if (game.startGame) {
+        let newGame
         switch (e.key) {
             case "a":
                 game.moveLeft(game.player1)
+                newGame = { ...game }
+                newGame.player2.position = undefined
+                newGame.player2.coins = undefined
+                socket.emit('game', newGame)
+
                 break;
 
             case 'd':
                 game.moveRight(game.player1)
+                newGame = { ...game }
+                newGame.player2.position = undefined
+                newGame.player2.coins = undefined
+                socket.emit('game', newGame)
                 break;
 
             case 'w':
                 game.moveUp(game.player1)
+                newGame = { ...game }
+                newGame.player2.position = undefined
+                newGame.player2.coins = undefined
+                socket.emit('game', newGame)
                 break;
 
             case 's':
                 game.moveDown(game.player1)
+                newGame = { ...game }
+                newGame.player2.position = undefined
+                newGame.player2.coins = undefined
+                socket.emit('game', newGame)
                 break;
 
             case "j":
                 game.moveLeft(game.player2)
+                newGame = { ...game }
+                newGame.player1.position = undefined
+                newGame.player1.coins = undefined
+                socket.emit('game', newGame)
                 break;
 
             case 'l':
                 game.moveRight(game.player2)
+                newGame = { ...game }
+                newGame.player1.position = undefined
+                newGame.player1.coins = undefined
+                socket.emit('game', newGame)
                 break;
 
             case 'i':
                 game.moveUp(game.player2)
+                newGame = { ...game }
+                newGame.player1.position = undefined
+                newGame.player1.coins = undefined
+                socket.emit('game', newGame)
                 break;
 
             case 'k':
                 game.moveDown(game.player2)
+                newGame = { ...game }
+                newGame.player1.position = undefined
+                newGame.player1.coins = undefined
+                socket.emit('game', newGame)
                 break;
         }
-        socket.emit('game',game)
-        
+
+
     }
 })
 
@@ -56,19 +90,19 @@ socket.on('game', function (newGame) {
     if (!game.startGame) {
         game.generateGame(newGame.rows, newGame.columns)
     }
-    game.board.matrix= newGame.board.matrix
+    game.board.matrix = newGame.board.matrix
     game.columns = newGame.columns
     game.rows = newGame.rows
-    game.player1.coins = newGame.player1.coins
-    game.player2.coins = newGame.player2.coins
-    game.player1.position = newGame.player1.position
-    game.player2.position = newGame.player2.position
+    game.player1.coins = newGame.player1.coins || game.player1.coins
+    game.player2.coins = newGame.player2.coins || game.player2.coins
+    game.player1.position = newGame.player1.position || game.player1.position
+    game.player2.position = newGame.player2.position || game.player2.position
     game.startGame = newGame.startGame
     game.totalCoins = newGame.totalCoins
-    
+
     renderer.render(game.getBoard())
-        renderer.renderScore(game.getCoins())
-        if (game.checkGameOver()) {
-            renderer.renderGameOver(game.getWinner())
-        }
+    renderer.renderScore(game.getCoins())
+    if (game.checkGameOver()) {
+        renderer.renderGameOver(game.getWinner())
+    }
 })
